262bed14c0c3f8532d8d950986d632ab09ec4fcd
erofili1 <102244993+erofili1@users.noreply.github.com>
2022-05-16 13:18:34 +0100

    Search button - preventDefault()

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+function searchCity(event) {
+    event.preventDefault();
+}
+
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", searchCity);
+
+
+
 function displayTemperature(response) {
     let temperature = Math.round(response.data.main.temp);
     let temperatureValue = document.querySelector("#temperature-value");
@@ -5,7 +14,7 @@ function displayTemperature(response) {
 }
 
 const apiKey = "7654bb3646824703bcfdf4ced8409f03";
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chania&appid=${apiKey}&units=metric`;
+let city = "Chania";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
