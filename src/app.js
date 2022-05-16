e73e4913588689ad5647e8aa9db8ffce75da798a
erofili1 <102244993+erofili1@users.noreply.github.com>
2022-05-16 18:08:59 +0100

    cleaner code

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ function displayTemperature(response) {
         let descriptionElement = document.querySelector("#description");
         let humidityElement = document.querySelector("#humidity");
         let windElement = document.querySelector("#wind");
+        
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
         cityElement.innerHTML = response.data.name;
         descriptionElement.innerHTML = response.data.weather[0].description;
@@ -64,11 +65,12 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
 
 let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ]
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+//let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 let now = new Date();
 let year = now.getFullYear();
 let day = weekdays[now.getDay()];
-let month = months[now.getMonth()];
+let date = now.getDate();
+let month = now.getMonth();
 let hour = now.getHours();
 let minutes = now.getMinutes().toString().padStart(2, "0");
 
@@ -76,3 +78,4 @@ console.log(minutes);
 
 let currentDateTime = document.querySelector("#date-time");
 currentDateTime.innerHTML = `${day} ${hour}:${minutes}`;
+//${date}/${month}/${year}`;
