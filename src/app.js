a90a06a776a2321a4a0b756dae0fa4bb73523215
erofili1 <102244993+erofili1@users.noreply.github.com>
2022-05-16 14:23:29 +0100

    Id updates

--- src/app.js
+++ src/app.js
@@ -2,24 +2,25 @@
 
 function displayTemperature(response) {
     let temperature = Math.round(response.data.main.temp);
-    let temperatureValue = document.querySelector("#temperature-value");
+    let temperatureValue = document.querySelector("#temperature");
     temperatureValue.innerHTML = temperature;
+    
 }
 
 
 function search(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#text-search-input");
-    let cityName = document.querySelector("#display-city-name");
+    let cityName = document.querySelector("#city");
     cityName.innerHTML = searchInput.value;
-
-    let apiKey = "7654bb3646824703bcfdf4ced8409f03";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
+    
 }
-
-
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+
+let city = "Chania";
+let apiKey = "7654bb3646824703bcfdf4ced8409f03";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
