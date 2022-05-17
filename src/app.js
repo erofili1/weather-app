ca5a0724370c06b5a654fafd15ddf30909433420
erofili1 <102244993+erofili1@users.noreply.github.com>
2022-05-17 17:33:53 +0100

    icon alt updated

--- src/app.js
+++ src/app.js
@@ -48,6 +48,7 @@ function displayTemperature(response) {
         
         
         iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+        iconElement.setAttribute("alt", response.data.weather[0].description);
 
         
         function celsiusToFahr(event) {
